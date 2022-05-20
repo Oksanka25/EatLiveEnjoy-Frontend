495c095b957d157a27f495f4dd9fd675ad19574e
Oksanka25 <chekoksa25@gmail.com>
2022-05-19 20:22:45 -0400

    Removed extra files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-// import logo from './logo.svg';
+
 import './App.css';
 import Header from "./components/Header";
-// import Footer from "./components/Footer";
-
 import Main from './components/Main';
 
 
 
-function App(props) {
+function App() {
 
   return (
     <div>
       <Header />
-      {/* <Home /> */}
       <Main />
-      {/* <Footer /> */}
     </div>
   );
 }
