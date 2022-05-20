f7ef57a7a3cf76418641513f04c07c0ec923072f
Oksanka25 <chekoksa25@gmail.com>
2022-05-19 21:43:50 -0400

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className=' footer p-2 mt-5 mb-4 me-0 d-flex justify-content-center'>
+            Created by Oksana Kalyniak
+        </div>
+    )
+}
+
+export default Footer
